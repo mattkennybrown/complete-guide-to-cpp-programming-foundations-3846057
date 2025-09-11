595484120c7d5ecf34c4d9b738302efbcedb85a2
Matthew Brown <m.brown.12@bham.ac.uk>
2025-09-11 14:18:59 +0100

    Finish 01_02

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,15 @@
 // Complete Guide to C++ Programming Foundations
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+
+#include <iostream>
+
+int main(){
+    // std::endl is a manipulator that inserts a newline character and flushes the stream
+    // end in ; 
+    std::cout << "Hello World!" << std::endl;
+    // return 0 indicates that the program ended successfully, no errors
+    // two new lines for better readability in terminal
+    std::cout << std::endl << std::endl; 
+    return 0;
+}
