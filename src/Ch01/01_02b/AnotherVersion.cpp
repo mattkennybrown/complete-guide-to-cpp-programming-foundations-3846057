a8d8f05a7050509088664b194e13db4de5338afd
Matthew Brown <m.brown.12@bham.ac.uk>
2025-09-11 14:31:39 +0100

    Other versions of simple scripts

---
+++ src/Ch01/01_02b/AnotherVersion.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+/* This is how to add a multi
+    line message*/
+
+int main() { // This is the main function
+    std::cout << "Hello World!" << std::endl; // Print Hello World!
+    return 0; // End of the main function
+    }
---
+++ src/Ch01/01_02b/CLIexample.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+//command line arguments in the main function
+int main(int argc, char* argv[]){
+    // argc is the number of command line arguments
+    // argv is an array of c-strings representing the arguments
+    std::cout << "Hello World!" << std::endl;
+    return 0;
+}
