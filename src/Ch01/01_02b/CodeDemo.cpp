a7ca2061526cf7fde3714293f71b753930c38f65
Matthew Brown <m.brown.12@bham.ac.uk>
2025-09-11 14:25:37 +0100

    More comments

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -2,11 +2,13 @@
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
 
+// directive to include the iostream header file into the program
 #include <iostream>
 
 int main(){
-    // std::endl is a manipulator that inserts a newline character and flushes the stream
-    // end in ; 
+    // statement -  the line saying what to do, ends in ;
+    // std::endl is a manipulator that inserts a newline character and flushes the stream (like enter)
+    // << directs the output to the standard output stream (console)
     std::cout << "Hello World!" << std::endl;
     // return 0 indicates that the program ended successfully, no errors
     // two new lines for better readability in terminal
